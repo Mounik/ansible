a4b27ef12e1f31bf4965ad09bab8757de7aa1406
Mounik <mounicou@gmail.com>
2023-07-24 13:59:22 +0200

    On garde les playbook par modules

--- deploy-ansible.sh
+++ deploy-ansible.sh
@@ -44,7 +44,7 @@ createContainers(){
   
 	# Création des conteneurs en boucle
 	for i in $(seq $id_min $id_max);do
-		sudo podman run -d --systemd=true --publish-all=true -v /srv/data:/srv/data --name ${CONTAINER_USER}-$i -h ${CONTAINER_USER}-$i docker.io/mounik/debian_12
+		sudo podman run -d --systemd=true --publish-all=true -v ./data:/srv/data --name ${CONTAINER_USER}-$i -h ${CONTAINER_USER}-$i docker.io/mounik/debian_12
 		${CONTAINER_CMD} ${CONTAINER_USER}-$i /bin/sh -c "useradd -m -p sa3tHJ3/KuYvI ${CONTAINER_USER}"
 		${CONTAINER_CMD} ${CONTAINER_USER}-$i /bin/sh -c "mkdir -m 0700 ${CONTAINER_HOME}/.ssh && chown ${CONTAINER_USER}:${CONTAINER_USER} ${CONTAINER_HOME}/.ssh"
 		sudo podman cp ${HOME}/.ssh/id_rsa.pub ${CONTAINER_USER}-$i:${CONTAINER_HOME}/.ssh/authorized_keys
